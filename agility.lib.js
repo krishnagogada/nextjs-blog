f9cba3a8ff4a57bdb16cd6838b7f4b3f6ceb0531
bkv <krishna.gogada@nxtwave.co.in>
2024-11-29 13:04:48 +0530

    Hard code api key

--- agility.lib.js
+++ agility.lib.js
@@ -3,6 +3,7 @@ const instanceGuid = "9c155e60-u";
 
 export const api = agility.getApi({
   guid: instanceGuid,
-  apiKey: process.env.AGILITY_API_KEY,
+  apiKey:
+    "defaultlive.ecf20361457b07ef487682bcd56f757f7ff67c48fae59b8dc68d6842d58bcaad",
   isPreview: false,
 });
